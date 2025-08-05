79941b71cd3e0e119df961d9798e6b5f4c145bb5
Tehseen1315 <156634158+Tehseen1315@users.noreply.github.com>
2025-08-05 00:02:43 -0400

    Update

--- Javascript/15-AdditionalConcepts.js
+++ Javascript/15-AdditionalConcepts.js
@@ -7,3 +7,21 @@ function f1(){
 console.log(f1());
 
 // Number produced is between 0 and 1 but not 1 .
+
+//How to obtain number between 0 to 10 
+function f2(){
+    return Math.floor(Math.random() *10);
+}
+console.log(f2());
+
+// parseInt fn : Coverts string to integer 
+function convert(int){
+    return parseInt(int);
+} 
+console.log(convert("56"));
+
+// Using parseInt function with radix
+function binaryToInt(int){
+    return parseInt(int);
+}
+console.log(parseInt("10011",2)); // 2 is the base ; 10 is the default base
